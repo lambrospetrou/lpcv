ba57883a807fcb2e5dfaa60c5d30431a4c1fc150
Lambros Petrou <lambrospetrou@gmail.com>
2017-11-16 17:35:09 +0000

    Made the PDF generation to use the local build instead of remote website

--- html/build-tools/generate-pdf.js
+++ html/build-tools/generate-pdf.js
@@ -1,10 +1,24 @@
 const puppeteer = require('puppeteer');
 
-(async() => {
+//////////////// Start a server to serve the local built CV
+const serve = require('serve');
+const path = require('path');
 
+const server = serve(path.join(__dirname, "../build"), {
+    port: 12345,
+    silent: true
+});
+
+console.info("Generating PDF...");
+
+setTimeout(() => generatePdf(server), 1000);
+
+/// Do the PDF printing!
+
+async function generatePdf(server) {
 const browser = await puppeteer.launch();
 const page = await browser.newPage();
-await page.goto('https://www.lambrospetrou.com/cv/', {waitUntil: 'networkidle2'});
+await page.goto('http://127.0.0.1:12345', {waitUntil: 'networkidle2'});
 await page.pdf({
     path: 'cv.pdf', 
     format: 'A4',
@@ -17,4 +31,8 @@ await page.pdf({
 });
 
 browser.close();
-})();
+
+// Close the server hosting the CV
+server.stop();
+
+}
